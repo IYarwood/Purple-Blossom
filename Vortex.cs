c234145363cc6b23976ec3914b8ec8be740c5881
IYarwood <117126660+IYarwood@users.noreply.github.com>
2024-02-26 21:30:24 -0500

    My files

---
+++ Vortex.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vortex : MonoBehaviour
+{
+
+    public GameObject model;
+    public GameObject secondaryModel;
+    private bool state = true;
+
+    private float timeSinceActivation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            if (state == true)
+            {
+                model.SetActive(false);
+                state = false;
+                secondaryModel.SetActive(true);
+                timeSinceActivation = Time.time;
+            }
+        }
+
+        if (Time.time - timeSinceActivation >= 2)
+        {
+            model.SetActive(true);
+            secondaryModel.SetActive(false);
+            state = true;
+        }
+        
+
+    }
+}
